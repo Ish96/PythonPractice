0563fa97cc915aabb22d1f97de402b221fc665ce
Ishan Shah <imshah96@gmail.com>
2021-02-17 17:45:35 +0530

    First using local control system

---
+++ Problem2/ArrayProblem1.py
@@ -0,0 +1,13 @@
+n = int(input("Enter the number:"))
+ls = []
+mul = 1
+if n < 4:
+    print("Invalid Input")
+else:
+    print("Enter" + str(n) + "numbers:")
+    for i in range(n):
+        ls.append(int(input()))
+    ls.sort()
+    for i in range(4):
+        mul = mul * ls[i]
+    print(mul)
